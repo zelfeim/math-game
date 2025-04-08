82be9cda8770b2bb8f7ab98d15a107e5c54b558c
zelfeim <zelfeim@mailbox.org>
2025-04-09 00:20:54 +0200

    Add TextMesh Pro and Operation types

---
+++ Assets/Scripts/Game/Misc/TextController.cs
@@ -0,0 +1,30 @@
+using System;
+using Game.Player;
+using TMPro;
+using UnityEngine;
+
+namespace Game.Misc
+{
+    public class TextController : MonoBehaviour
+    {
+        private TextMeshProUGUI _text;
+
+        private void Start()
+        {
+            _text = GetComponent<TextMeshProUGUI>();
+            
+            PlayerController.OnScoreChange += UpdateScore;
+        }
+
+        private void OnDestroy()
+        {
+            PlayerController.OnScoreChange -= UpdateScore;
+        }
+
+        private void UpdateScore(double score)
+        {
+            // TODO: Round to .5?
+            _text.SetText(score.ToString("F1"));        
+        }
+    }
+}
